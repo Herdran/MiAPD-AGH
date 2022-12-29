8257ebd2bf4b896c351eb100c9bcce0f22c22217
Herdran <jakub4426@gmail.com>
2022-12-29 23:47:37 +0100

    GUI update

--- Decision making - Choosing game console/GUI/gui.py
+++ Decision making - Choosing game console/GUI/gui.py
@@ -1,22 +1,13 @@
+import itertools
+import os
 import pathlib
 
 from kivy.config import Config
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
 from kivy.properties import StringProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.checkbox import CheckBox
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.slider import Slider
 from kivymd.app import MDApp
-from kivymd.uix.list import MDList, OneLineListItem, OneLineAvatarListItem
-import os
-
-from kivymd.uix.selectioncontrol import MDCheckbox
-from kivymd.uix.slider import MDSlider
 
 Config.set("input", "mouse", "mouse,disable_multitouch")
 
@@ -24,71 +15,89 @@ file_path = str(pathlib.Path(__file__).parent.resolve())
 
 ahp = None
 
+criteria = ["Price", "Graphics", "Performance"]
 
-# from ..AHP.ahp_thread_placeholder import complicated_stuff
+alternatives = ["PS4", "PS5", "XBOX ONE", "XBOX SERIES X", "XBOX SERIES S", "NINTENDO SWITCH", "STEAM DECK"]
 
+images_path = os.path.join(file_path, "Images")
 
-# def on_checkbox_active(self, checkbox, value):
-#     if value:
-#         print('The checkbox', checkbox, 'is active')
-#     else:
-#         print('The checkbox', checkbox, 'is inactive')
+alternatives_images_links = {alternative: alternative.replace(" ", "_") + ".png" for alternative in alternatives}
+
+alternatives_combinations = list(itertools.combinations(alternatives, 2))
+
 
 class WelcomeScreen(Screen):
     pass
 
 
 class MainScreen(Screen):
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #     self.curr_val = 0
-    # activeee = False
+    curr_val = 0
+    curr_criterion_index = 0
+    curr_alternatives_index = -1
+
+    criteria_count = len(criteria)
+    alternatives_combinations_count = len(alternatives_combinations)
+
+    curr_criterion = StringProperty(criteria[curr_criterion_index])
+    left_text = StringProperty(alternatives_combinations[curr_alternatives_index][0])
+    right_text = StringProperty(alternatives_combinations[curr_alternatives_index][1])
+    left_image_link = StringProperty(
+        os.path.join(images_path, alternatives_images_links[alternatives_combinations[curr_alternatives_index][0]]))
+    right_image_link = StringProperty(
+        os.path.join(images_path, alternatives_images_links[alternatives_combinations[curr_alternatives_index][1]]))
 
     def set_curr_val(self, val):
-        # print(self.curr_val)
-        # complicated_stuff(self.curr_val)
-        ahp.set_curr_val(val)
+        self.curr_val = val
 
-    def get_curr_val(self):
-        ahp.get_curr_val()
+    def confirm_answer(self):
+        print(self.curr_val)
+        self.change_question()
 
-    def reset_checkbox(self):
-        # self.activeee = False
+    def skip_question(self):
+        print("Skipping question? idk how would that work")
+        self.change_question()
 
-        for child in reversed(self.ids.grid.children):
-            # print(child)
-            if isinstance(child, MDCheckbox):
-                child.active = False
-                print("test")
+    def change_question(self):
+        if self.curr_alternatives_index >= self.alternatives_combinations_count - 1:
+            if self.curr_criterion_index >= self.criteria_count - 1:
+                self.curr_criterion_index = 0
+                self.curr_alternatives_index = 0
+                print("All questions answered, displaying results")
+                self.manager.current = "results_screen"
+            else:
+                self.curr_criterion_index += 1
+                self.curr_alternatives_index = 0
+        else:
+            self.curr_alternatives_index += 1
+
+        self.curr_criterion = criteria[self.curr_criterion_index]
+        self.left_text = alternatives_combinations[self.curr_alternatives_index][0]
+        self.right_text = alternatives_combinations[self.curr_alternatives_index][1]
+        self.left_image_link = os.path.join(images_path, alternatives_images_links[self.left_text])
+        self.right_image_link = os.path.join(images_path, alternatives_images_links[self.right_text])
+
+    # def reset_checkbox(self):
+    #     for child in reversed(self.ids.grid.children):
+    #         # print(child)
+    #         if isinstance(child, MDCheckbox):
+    #             child.active = False
+    #             print("test")
 
 
 class SettingsScreen(Screen):
     pass
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
 
-    # def slider_ring_change_blocker(self, instance, value):
-    #     if instance.value == instance.min:
-    #         instance._is_off = False
-    #     # instance.text = "something different"
-    # def on_checkbox_active(self, checkbox, value):
-    #     if value:
-    #         print('The checkbox', checkbox, 'is active')
-    #     else:
-    #         print('The checkbox', checkbox, 'is inactive')
+
+class ResultsScreen(Screen):
+    pass
 
 
 class WindowManager(ScreenManager):
-    images_path = os.path.join(file_path, "Images")
     screen_mode_change_image_link = StringProperty(os.path.join(images_path, "fullscreen_maximize.png"))
     settings_icon_link = StringProperty(os.path.join(images_path, "settings.png"))
     return_icon_link = StringProperty(os.path.join(images_path, "return.png"))
     skip_icon_link = StringProperty(os.path.join(images_path, "next.png"))
-    button_rectangle_image_link = StringProperty(os.path.join(images_path, "button_rectangle_round_corners_background.png"))
-    left_image_link = StringProperty(os.path.join(images_path, "left_image_placeholder.png"))
-    right_image_link = StringProperty(os.path.join(images_path, "right_image_placeholder.png"))
     last_screen = "welcome_screen"
-
 
     def screen_mode_change(self):
         if Window.fullscreen:
@@ -100,18 +109,9 @@ class WindowManager(ScreenManager):
 
 
 class Gui(MDApp):
-    # def on_checkbox_active(self, checkbox, value):
-    #     if value:
-    #         print('The checkbox', checkbox, 'is active')
-    #     else:
-    #         print('The checkbox', checkbox, 'is inactive')
-
     def build(self):
         Window.minimum_width = 640
         Window.minimum_height = 360
         self.icon = file_path + "Images//logo_maybe.png"
         self.title = "Decision making - Choosing game console"
         return Builder.load_file(os.path.join(file_path, "gui.kv"))
-
-# if __name__ == "__main__":
-#     Gui().run()
--- Decision making - Choosing game console/main.py
+++ Decision making - Choosing game console/main.py
@@ -1,11 +1,6 @@
-import threading
-
 from GUI import gui
-# from AHP.ahp_thread_placeholder import complicated_stuff
-from AHP import ahp_thread_placeholder
+from AHP import ahp
 
 if __name__ == "__main__":
-    # ahp_thread = threading.Thread(target=ahp_thread_placeholder, args=())
-    # ahp_thread.start()
-    gui.ahp = ahp_thread_placeholder.ahp()
+    gui.ahp = ahp
     gui.Gui().run()
